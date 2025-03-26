8267eedfbf470cbad0f3fc25fe79401ca474e993
Emiliano Pérez <emilianojesusp599@gmail.com>
2025-03-25 19:53:05 -0600

    Configuración de Chat Screen y ListView

---
+++ lib/presentation/chat/chat_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://static.vecteezy.com/system/resources/previews/007/225/199/original/robot-chat-bot-concept-illustration-vector.jpg'),
+          ),
+        ),
+        title: Text('Chat Roboboy'),
+        centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return Text('Indice: $index');
+              },
+            )),
+            Text('Mundo')
+          ],
+        ),
+      ),
+    );
+  }
+}
